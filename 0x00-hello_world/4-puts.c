1e5fb9b48fdbd8078456543f375532d134fa6ec1
4DIL <al.a24dil@gmail.com>
2023-06-15 07:54:32 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle"
+ * main - prints "Programming is like building a multilingual puzzle
+ * followed by a newline"
  * Return : Always 0
+ *
  */
 
 int main(void)
