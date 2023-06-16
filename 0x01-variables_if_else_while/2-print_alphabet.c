1f3d138463abb91608a22531f3cf9d9fdc7c85db
4DIL <al.a24dil@gmail.com>
2023-06-16 13:39:17 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ *  followed by new line
+ *  Return: Always 0
+ */
+int main(void)
+{
+	char x;
+	
+	for (x == 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
