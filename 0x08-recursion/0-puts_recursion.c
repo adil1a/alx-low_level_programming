cfb09b4d3e2f9e0b1a210d0ddeee3520977abac3
adil1a <al.a24dil@gmail.com>
2023-10-16 17:01:23 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts_recursion - prints a string, followed by /n
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
